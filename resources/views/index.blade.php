874f3b95fe62a9f9eb3f26568820d879d7d4f58f
Luis Manuel Alvarez Bautista <lmalvarez@personalsoft.com.co>
2021-03-21 19:28:27 -0500

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.web')
+@section('content')
+    <div class="text-center">
+        <h1 class="text-3xl text-gray-700 mb-2 uppercase">Últimos curos</h1>
+        <h2 class="text-xl text-gray-700">Fórmate como profesional en tecnología</h2>
+        <h3 class="text-lg text-gray-700">70% de los cuadrados duplican sus ingresos</h3>
+    </div>
+    <livewire:course-list>
+@endsection
